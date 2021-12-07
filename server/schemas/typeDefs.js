20f616d734f7d963bca5ffa7debe1014322dfd57
Edward Castaneda <aeghin7@gmail.com>
2021-12-07 17:52:22 -0600

    qgl files created and seeded

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+ 
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+  
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: savedBook!): User
+    removeBook(bookId: ID!): User
+  }
+  input savedBook {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+`;
+
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express')
+const { authMiddleware } = require("./utils/auth");
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
-  
+  typeDefs,
+  resolvers,
+  context: authMiddleware
 })
 
 server.applyMiddleware({ app });
